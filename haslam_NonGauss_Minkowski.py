20873151496e3d30308563dec3d26d30c735bbc1
Sandeep <sandeepranaiiser@gmail.com>
2018-03-30 03:32:21 +0000

    Version one for Minkowski functional for haslm gauss Maps

--- haslam_NonGauss_Minkowski.py
+++ haslam_NonGauss_Minkowski.py
@@ -12,60 +12,56 @@ Jens Schmalzing et. al. 1998
 
 import numpy as np
 import healpy as hp
-import matplotlib.pyplot as plt
+from  multiprocessing import Process
 from scipy import special
+
+
+# Global values
+nlmax = 256
+Nside =128
+Npix = hp.nside2npix(Nside)
+cosbysin = np.zeros(Npix)
+for ipix in xrange(Npix):
+    theta, phi = hp.pix2ang(Nside, ipix)
+    cosbysin[ipix] = np.cos(theta)/np.sin(theta)
 
 
 #++++++++++++++++++++ l Filter ++++++++++++++++++++++
 """
-In this section I am Generating
-
+In this section I am Generating filter function
+anchoring at diffetent l0 value such that
+if l<= l0, f = 0
+if l> l0 , f = 1
+Filter function written in a way that it won't go
+zero sharply but rather in a smooth fashion
 """
+l0 = [10, 20, 40 , 80, 120]
 
+def filter_arr1(ini, final):
+    delta_l = 5
+    window_func = np.zeros(nlmax, dtype=np.float)
+    for l in xrange(ini, final):
+        if ini + delta_l <= l :
+            window_func[l] = 1.0
 
+        elif ini <= l < ini + delta_l:
+            window_func[l] = np.cos(np.pi * 0.5 * ((ini + delta_l) - l) / delta_l) ** 2
 
+    return window_func
 
+window_func_filter = np.zeros((len(l0), nlmax), dtype=np.float)
 
+for i in xrange(len(l0)):
+    ini = l0[i]
+    window_func_filter[i, :] = filter_arr1(ini, nlmax)
 
+#++++++++++++++++++++ end l Filter ++++++++++++++++++++++
 
+#++++++++++++++ Analytical Form of Scalar Minkowski ++++++
 
+def analaytic_S0(mean_g, x, Sigma):
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Nside =512
-
-cl = hp.fitsfunc.read_cl("cl.fits")
-
-gauss_map = hp.synfast(cl[0], 512, lmax=3*Nside-1, fwhm=0, pixwin=True, verbose=False, pol=False)
-
-def analaytic_S2(mean_g, x, tau, Sigma):
-
-    temp =  (tau/Sigma) * (x - mean_g)/np.sqrt(2*Sigma) * (1./ (2.*np.pi**1.5))
-
-    return temp * np.exp(- ((x - mean_g)**2.) /2/Sigma)
+    return 0.5*special.erfc( (x - mean_g) / np.sqrt(2.*Sigma) )
 
 def analaytic_S1(mean_g, x, tau, Sigma):
 
@@ -73,162 +69,189 @@ def analaytic_S1(mean_g, x, tau, Sigma):
 
     return temp * np.exp(- ((x - mean_g)**2.) /2/Sigma)
 
+def analaytic_S2(mean_g, x, tau, Sigma):
 
-def analaytic_S0(mean_g, x, Sigma):
+    temp =  (tau/Sigma) * (x - mean_g)/np.sqrt(2*Sigma) * (1./ (2.*np.pi**1.5))
 
-    return 0.5*special.erfc( (x - mean_g) / np.sqrt(2.*Sigma) )
+    return temp * np.exp(- ((x - mean_g)**2.) /2/Sigma)
 
-
-
-#+++++++++++++++++++++++++++++++++++++++++++
-
-G_mean = np.mean(gauss_map)
-sigma = np.std(gauss_map)
-Npix = hp.nside2npix(Nside)
-
-#print Npix
+#++++++++++++++ End analytical Form of Scalar Minkowski ++++++
 
 
-#================================================================dd
-g_map = (gauss_map-G_mean)/sigma
+def Map_Prep(inp_map, Sky_mask, lFilter):
+    """
+    # Map prepration
+    param1 :- Input Map
+    param2 :- galactic Mask
+    returns
+    1 : Scalar u field
+    2 : grad|u|
+    3 : grad|u|*Kappa
+    """
 
-#Cl = hp.anafast(g_map, lmax=3*Nside-1)
-#ell = np.arange(len(Cl))
+    inp_map = inp_map*Sky_mask
+    inp_map_alm = hp.map2alm(inp_map, lmax=nlmax)
+
+    inp_map = hp.alm2map(hp.almxfl(inp_map_alm*lFilter), Nside)
+
+    inp_map_mean = np.mean(inp_map)
+    inp_map_sigma = np.std(inp_map)
+
+    g_map = (inp_map-inp_map_mean)/inp_map_sigma # we calling it g_map where in Jens Schmalzing et. al. 1998 its u(generic scalar field)
+
+    g_alm = hp.sphtfunc.map2alm(g_map, lmax=nlmax)
+    g_map, d_theta_g, d_phi_g = hp.sphtfunc.alm2map_der1(g_alm, Nside)
 
 
-cosbysin = np.zeros(Npix)
-for ipix in xrange(Npix):
-    theta, phi = hp.pix2ang(Nside, ipix)
-    cosbysin[ipix] = np.cos(theta)/np.sin(theta)
+    #Gradient of gmap grad|g|
+    grad_g_map = np.sqrt(d_theta_g**2 + d_phi_g**2)/4
+
+    # Computing Kappa
+    u1 = d_theta_g
+    u2 = d_phi_g
 
 
+    d_theta_g_alm = hp.sphtfunc.map2alm(d_theta_g)
+    d_phi_g_alm = hp.sphtfunc.map2alm(d_phi_g)
 
-g_alm = hp.sphtfunc.map2alm(g_map, lmax=3*Nside-1)
-g_map, d_theta, d_phi = hp.sphtfunc.alm2map_der1(g_alm, Nside)
+    d_phi_g, Du_theta_phi_g, Du_phi_phi_g = hp.sphtfunc.alm2map_der1(d_phi_g_alm, Nside)
+    d_theta_g, u11, Du_theta_phi_g = hp.sphtfunc.alm2map_der1(d_theta_g_alm, Nside)
 
+    u12 = Du_theta_phi_g - cosbysin * d_phi_g
 
+    u22 = Du_phi_phi_g + cosbysin* d_theta_g
 
-grad_gauss_map = np.sqrt(d_theta**2 + d_phi**2)/4
+    kapa = (2* u1 * u2* u12 - u1**2 * u22 - u2**2 * u11)
+    kapa /= (u1**2 + u2**2)
+    kapa /= 2*np.pi
 
-u1 = d_theta
-u2 = d_phi
-
-
-d_theta_alm = hp.sphtfunc.map2alm(d_theta)
-d_phi_alm = hp.sphtfunc.map2alm(d_phi)
-
-d_phi, Du_theta_phi, Du_phi_phi = hp.sphtfunc.alm2map_der1(d_phi_alm, Nside)
-d_theta, u11, Du_theta_phi = hp.sphtfunc.alm2map_der1(d_theta_alm, Nside)
-
-u12 = Du_theta_phi - cosbysin * d_phi
-u22 = Du_phi_phi + cosbysin* d_theta
-
-kapa = (2* u1 * u2* u12 - u1**2 * u22 - u2**2 * u11)
-kapa /= (u1**2 + u2**2)
-kapa /= 2*np.pi
-
+    return g_map, grad_g_map, kapa
 
 #=================================================================
 
-mean_ana = np.mean(g_map)
-Sig_Sum = (np.mean(g_map**2) -np.mean(g_map)**2 )
-tau_Sum = 0.5*(np.mean(d_theta*d_theta + d_phi*d_phi))
+def thresh_masking(inp_mask):
+    """
+    param:-  Input Mask(apodize)
+    return:- binary mask with some threshold value
+    chosen fby us in this case I am taking 0.998
+    """
+    binary_mask = np.zeros(len(inp_mask))
 
-#Sig_Sum = 0.
-#tau_Sum = 0.
-#for i in xrange(len(Cl)):
-#     Sig_Sum = Sig_Sum + (2*ell[i]+1)*Cl[i]
-#     tau_Sum = tau_Sum + (2*ell[i]+1)*Cl[i] * (0.5*ell[i]*(ell[i]+1))
-#print SigmaO, Sigma1
-
-delta = 0.2
-nu = np.arange(-4, 4., delta)
-
-SO = np.zeros(len(nu)-1)
-S1 = np.zeros(len(nu)-1)
-S2 = np.zeros(len(nu)-1)
-Ana2 = np.zeros(len(nu)-1)
-Ana1 = np.zeros(len(nu)-1)
-Ana0 = np.zeros(len(nu)-1)
-
-for i in xrange(len(nu)-1):
-
-    index = (g_map>nu[i])*(g_map<nu[i+1])
-    index1 = (g_map>nu[i])
-
-    temp1 = g_map[index1]
-    temp2 = grad_gauss_map[index]
-    temp3 = kapa[index]
-
-    nu_mean = (nu[i+1]+nu[i])/2.
-    Ana0[i] = analaytic_S0(mean_ana, nu_mean, Sig_Sum)
-    Ana1[i] = analaytic_S1(mean_ana, nu_mean, tau_Sum, Sig_Sum)
-    Ana2[i] = analaytic_S2(mean_ana, nu_mean, tau_Sum, Sig_Sum)
-
-    SO[i] = (len(temp1))/(Npix*1.0)
-    S1[i] = (np.sum(temp2)/delta)/(Npix*1.0)
-    S2[i] = (np.sum(temp3)/delta)/(Npix*1.0)
-
-plt.style.use('classic')
-
-plt.figure(1, figsize=(8, 6))
-plt.plot(nu[:-1], SO, 'o' , color='orange', alpha=0.8, label=r'$Data$')
-plt.plot(nu[:-1], Ana0, linestyle='-' , color='orange',linewidth=3, alpha=0.5, label=r'$Analytical$')
-
-plt.legend(loc=2, fontsize=14, frameon=False)
-plt.minorticks_on()
-plt.ylim(-0.001, 1.05)
-plt.xlabel(r'$S_{1}$', fontsize=14)
-plt.ylabel(r'$\nu$', fontsize=14)
-plt.tick_params(axis='both', which='minor', length=4, width=2, labelsize=12)
-plt.tick_params(axis='both', which='major', length=8, width=3, labelsize=12)
-plt.tick_params(which='major', length=8, width=3, labelsize=12, right='on')
-plt.tick_params(which='minor', length=4, width=2, labelsize=12, right='on')
-plt.tick_params(which='minor', length=4, width=2, labelsize=12, top='on')
-plt.tick_params(which='major', length=8, width=3, labelsize=12, top='on')
-plt.tight_layout()
-
-plt.savefig("S0.png", dpi=800)
+    for ipix in xrange(len(inp_mask)):
+        if inp_mask > 0.998:
+            binary_mask[ipix]=1
 
 
+    return binary_mask
+
+#====================================================================
+
+def compute_minkowski(Map, sky_mask, binary_temp_mask, fn):
+    """
+    """
+
+    delta = 0.2
+    nu = np.arange(-4, 4., delta)
+
+    S2 = np.zeros( (5, len(nu)-1))
+    Ana2 = np.zeros((5,len(nu)-1))
+
+    ind = (binary_temp_mask==1)
+    NPIX = binary_temp_mask[ind]
+    NPIX = len(NPIX)
 
 
-plt.figure(2, figsize=(8, 6))
-plt.plot(nu[:-1], S1, 'b o' , color='teal', alpha=0.8, label=r'$Data$')
-plt.plot(nu[:-1], Ana1, linestyle='-' , color='g',linewidth=3, alpha=0.5, label=r'$Analytical$')
-plt.legend(loc=2, fontsize=14, frameon=False)
-plt.minorticks_on()
-plt.xlabel(r'$S_{1}$', fontsize=14)
-plt.ylabel(r'$\nu$', fontsize=14)
-plt.tick_params(axis='both', which='minor', length=4, width=2, labelsize=12)
-plt.tick_params(axis='both', which='major', length=8, width=3, labelsize=12)
-plt.tick_params(which='major', length=8, width=3, labelsize=12, right='on')
-plt.tick_params(which='minor', length=4, width=2, labelsize=12, right='on')
-plt.tick_params(which='minor', length=4, width=2, labelsize=12, top='on')
-plt.tick_params(which='major', length=8, width=3, labelsize=12, top='on')
-plt.tight_layout()
+    for  l in xrange(0, 4):
+
+        u, grad_u, kapa_u = Map_Prep(Map, sky_mask, window_func_filter[l, :])
+
+        u     *= binary_temp_mask
+        grad_u *= binary_temp_mask
+        kapa_u *= binary_temp_mask
+
+        for j in xrange(len(nu)-1):
+
+#+++++++Analytic part+++++++++++++++
+
+            temp = grad_u**2*4
+            mean_ana = np.mean(u)
+            Sig_Sum = (np.mean(u**2) -np.mean(u)**2 )
+            tau_Sum = 0.5*(temp)
+            nu_mean = (nu[j+1]+nu[j])/2.
+            Ana2[l, j] = analaytic_S2(mean_ana, nu_mean, tau_Sum, Sig_Sum)
+
+#+++++++ Data part+++++++++++++++
+
+            index = (u  > nu[j])*(u  < nu[j+1])
+            temp3 = kapa_u[index]
+            S2[l, j] = (np.sum(temp3)/delta)/(NPIX*1.0)
+
+    fname1 = 'Gauss_haslam_Minkowski_functional_Anal2_%d.txt' % fn
+    fname2 = 'Gauss_haslam_Minkowski_functional_S2_%d.txt' % fn
+
+    np.save(fname1, zip(Ana2[0,:], Ana2[1,:], Ana2[2,:], Ana2[3,:], Ana2[4,:]),
+                    delimiter='\t',
+                    fmt='%0.6e\t%0.6e\t%0.6e\t%0.6e\t%0.6e'
+                    , header='l_10\tl_20\tl_40\tl_80\t_l_120')
 
 
-plt.savefig("S1.png", dpi=800)
-
-
-plt.figure(3, figsize=(8, 6))
-plt.plot(nu[:-1], S2, 'b o' , color='crimson', alpha=0.8, label=r'Data')
-plt.plot(nu[:-1], Ana2, linestyle='-' , color='crimson', linewidth=3, alpha=0.5, label=r'Analytical')
-plt.legend(loc=2, fontsize=14, frameon=False)
-plt.minorticks_on()
-plt.xlabel(r'$S_{2}$', fontsize=14)
-plt.ylabel(r'$\nu$', fontsize=14)
-plt.tick_params(axis='both', which='minor', length=4, width=2, labelsize=12)
-plt.tick_params(axis='both', which='major', length=8, width=3, labelsize=12)
-plt.tick_params(which='major', length=8, width=3, labelsize=12, right='on')
-plt.tick_params(which='minor', length=4, width=2, labelsize=12, right='on')
-plt.tick_params(which='minor', length=4, width=2, labelsize=12, top='on')
-plt.tick_params(which='major', length=8, width=3, labelsize=12, top='on')
-plt.tight_layout()
-
-plt.savefig("S2.png", dpi=800)
+    np.save(fname2, zip(S2[0,:], S2[1,:], S2[2,:], S2[3,:], S2[4,:]),
+                    delimiter='\t',
+                    fmt='%0.6e\t%0.6e\t%0.6e\t%0.6e\t%0.6e'
+                    , header='l_10\tl_20\tl_40\tl_80\t_l_120')
 
 
 
-plt.show()
+#====================================================================
+
+def parllel_compute(nmin, nmax, sky_Mask, b_mask):
+
+
+    for nn in xrange(nmin, nmax):
+        filename = '%d.fits'%nn
+        compute_minkowski(hp.read_map(filename), sky_Mask, b_mask, nn)
+
+#====================================================================
+
+def main():
+
+
+    TEMP = '60K'
+
+    name = '/jbodstorage/data_sandeep/sandeep/Bispectrum_data/Input_Maps/mask_apod_128/Mask_80K_apod_300arcm_ns_128.fits'
+    Mask_80K = hp.fitsfunc.read_map(name, verbose=False)
+
+
+    f_name1 = "/jbodstorage/data_sandeep/sandeep/Bispectrum_data/Input_Maps/mask_apod_128/Mask_%s_apod_300arcm_ns_128.fits" % TEMP
+    print f_name1
+    ap_mask_128 = hp.fitsfunc.read_map(name, verbose=False)
+    b_mask = thresh_masking(ap_mask_128)
+
+    max_core = 25
+    count=0
+    increment = 40
+    jobs = []
+
+    for i in xrange(0, max_core):
+        nmin = count
+        nmax = count + increment
+        if nmax == 1000:
+            nmax = 1001
+        s = Process(target=parllel_compute, args=(nmin, nmax, Mask_80K, b_mask))
+        jobs.append(s)
+        s.start()
+        count = nmax
+
+    for s in jobs:
+        s.join()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
